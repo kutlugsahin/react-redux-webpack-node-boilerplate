c495a2bff985ae927307f08194bd080936b827ed
ahmetkutlu <ku>
2016-07-08 18:26:34 +0300

    tabs indent

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,9 @@
 import React, {createClass} from 'react'
 
 var cls = {
-    backgroundColor: '#ccc',
-    height: '30px',
-    fontSize: '28px'
+	backgroundColor: '#ccc',
+	height: '30px',
+	fontSize: '28px'
 }
 
 import {render} from 'react-dom'
@@ -11,18 +11,18 @@ import {Provider} from 'react-redux'
 import store from './store/store.js'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+	constructor(props) {
+		super(props)
+	}
 
-    render() {
-        return (
-            <div>
-                <div style={cls}>Hello world!!.**.!!!!</div>
-                <div>Hello world!!!</div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<div style={cls}>Hello world!!.**.!!!!</div>
+				<div>Hello world!!!</div>
+			</div>
+		)
+	}
 }
 
 export default App;
